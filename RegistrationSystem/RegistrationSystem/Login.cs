9c2a1dc9120980a77e38cb530cbd52e967f3bcae
Kelvin Hu <hukelvin99@gmail.com>
2013-07-19 23:59:25 -0700

    Added Login.cs

---
+++ RegistrationSystem/RegistrationSystem/Login.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace RegistrationSystem
+{
+	public class Login
+	{
+		string Username, Password;
+		public Login(string Username, string Password)
+		{
+			this.Username = Username;
+			this.Password = Password;
+		}
+		void Check()
+		{
+
+		}
+	}
+}
+
